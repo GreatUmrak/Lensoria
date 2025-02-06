f4b8db7a0965ca34e869b345a6f75ebb00cb93de
GreatUmrak <egoumrak@mail.ru>
2025-02-06 19:21:07 +0300

    Переделка второй секции

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+    $('.card__image-link1').on('click', function(){
+        $('.card1').toggleClass('card--active');
+    });
+
+    $('.card__image-link2').on('click', function(){
+        $('.card2').toggleClass('card--active');
+    });
+
 });
