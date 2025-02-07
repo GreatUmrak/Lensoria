6e0837be8033113f6f6fb2e48b47f0f60f7a3592
GreatUmrak <egoumrak@mail.ru>
2025-02-07 16:22:49 +0300

    Переделана 3 секция и сделана 4 секция

--- js/main.js
+++ js/main.js
@@ -7,4 +7,13 @@ $(function(){
     $('.card__image-link2').on('click', function(){
         $('.card2').toggleClass('card--active');
     });
+    $('.vcc__right-text1').on('click', function(){
+        $('.vcc1').toggleClass('vcc--active');
+    });
+    $('.vcc__right-text2').on('click', function(){
+        $('.vcc2').toggleClass('vcc--active');
+    });
+    $('.vcc__right-text3').on('click', function(){
+        $('.vcc3').toggleClass('vcc--active');
+    });
 });
